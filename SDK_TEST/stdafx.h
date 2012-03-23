8e60f2cf70a299490288cd0171bf47cf7f9a58fa
Piotr Baniukiewicz <baniuk@zut.edu.pl>
2012-03-23 20:55:21 +0800

    poprawiony brak sdk_test

---
+++ SDK_TEST/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include "gtest/gtest.h"
+#include "C_Matrix_Container.h"
+#include "C_Error.h"
+
+
+// TODO: reference additional headers your program requires here
